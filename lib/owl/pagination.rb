e9255b6b87b67419429548e309ebdd0c78cf80da
vladislav <v.yashin.work@gmail.com>
2017-10-21 18:04:28 +0300

    Добавлена пагинация

---
+++ lib/owl/pagination.rb
@@ -0,0 +1,31 @@
+# Класс пагинации. Работает как с ActiveRecord::Relation, так и с массивами
+class Pagination
+  # Размер страницы по умолчанию
+  DEFAULT_PAGE_SIZE = 30
+
+  attr_accessor :page_size, :current_page, :pages_count, :items
+
+  # Инициализатор пагинации.
+  # @param items [ActiveRecord::Relation, Array] коллекция элементов для пагинации
+  # @option options [Integer] :page_size  размер страницы
+  # @option options [Integer] :current_page  номер текущей страницы
+  # @yieldparam [Pagination] pagination передаёт саму себя в переданный блок
+  # @example Создание пагинации
+  #   pagination = Pagination.new(Book.all, current_page: 3, page_size: 20)
+  #   pagination = Pagination.new(Book.all) do |pagination| pagination.page_size = 40 end
+  def initialize(items, options = {})
+    options.each { |k, v| instance_variable_set("@#{k.to_s}", v) }
+    yield self if block_given?
+
+    @page_size ||= DEFAULT_PAGE_SIZE
+    @current_page = 1 if @current_page.blank? || @current_page == 0
+
+    items_count = items.size
+    @pages_count = (items_count % @page_size == 0) ? items_count / @page_size : (items_count / @page_size) + 1
+    if items.respond_to? :limit
+      @items = items.limit(@page_size).offset(@page_size * (@current_page - 1))
+    elsif items.respond_to? :[]
+      @items = items.drop(@page_size * (@current_page - 1)).first(@page_size)
+    end
+  end
+end
