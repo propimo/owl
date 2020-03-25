694afe586d1df7f78320f06aa06aab3a767612f0
Dmitry Skvaznikov <orkich@gmail.com>
2020-03-25 10:01:00 +0400

    Вынести модуль упрощенной работы с AR

--- lib/owl.rb
+++ lib/owl.rb
@@ -1,4 +1,5 @@
 require 'active_record'
+require 'i18n'
 
 Dir[File.expand_path(File.join(File.dirname(File.absolute_path(__FILE__)), '/owl')) + "/**/*.rb"].each do |file|
   require file
@@ -11,4 +12,9 @@ module Owl
   BROWSERS = YAML::load_file(File.join(ROOT_PATH, 'config', 'browser_updater.yml'))
 
   require 'owl/railtie' if defined?(Rails)
+end
+
+ActiveSupport.on_load(:i18n) do
+  I18n.load_path << File.expand_path("../config/locales/en.yml", __dir__)
+  I18n.load_path << File.expand_path("../config/locales/ru.yml", __dir__)
 end
---
+++ lib/owl/engine.rb
@@ -0,0 +1,5 @@
+module Owl
+  class Engine < ::Rails::Engine
+    isolate_namespace Owl
+  end
+end
